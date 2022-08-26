0761cc42251d0d61d48572b181af59ce7b9657f3
availablegithubname <103818188+availablegithubname@users.noreply.github.com>
2022-08-26 17:17:02 +0200

    Update garbage.lua

--- garbage.lua
+++ garbage.lua
@@ -1,1 +1,2 @@
 if not shared.Garbage then loadstring(game:HttpGet("https://raw.githubusercontent.com/availablegithubname/GarbageHubForRoblox/main/GarbageHubMain.lua"))() return end
+-- funni line (i just broke ur script, cope)
