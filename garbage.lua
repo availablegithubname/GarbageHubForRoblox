d981f408eb5bfb5c814005014ba3f56b8dc459eb
availablegithubname <103818188+availablegithubname@users.noreply.github.com>
2022-08-26 17:19:07 +0200

    Update garbage.lua

--- garbage.lua
+++ garbage.lua
@@ -1,1 +1,2 @@
 if not shared.Garbage then loadstring(game:HttpGet("https://raw.githubusercontent.com/availablegithubname/GarbageHubForRoblox/main/GarbageHubMain.lua"))() return end
+-- LEAVE MY SCRIPTBREAKER (real) HERE, NO TOUCHING, ITS A CHILD!!
