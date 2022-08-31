d2bd87842ee16fc3d4ecb855d6346e27d778f40f
Milan08Studio <68651440+Milan08Studio@users.noreply.github.com>
2022-08-31 17:45:59 +0200

    Update GarbageHubMain.lua

--- GarbageHubMain.lua
+++ GarbageHubMain.lua
@@ -8,7 +8,6 @@ local handler = loadstring(game:HttpGet("https://raw.githubusercontent.com/avail
 queueteleport([[loadstring(game:HttpGet("https://raw.githubusercontent.com/availablegithubname/GarbageHubForRoblox/main/GarbageHubMain.lua"))()]])
 game.StarterGui:SetCore("SendNotification",{Title = "GarbageHub V3 Crasher",Text = ".crash <name> to lag someone out."})
 handler.e()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/availablegithubname/GarbageHubForRoblox/main/halal.lua"))()
 game.Players.PlayerChatted:Connect(function(_,sender,msg,_)
     if sender == game.Players.LocalPlayer and string.find(string.lower(msg), ".crash") then
         shared.Garbage.Target = game.Players:FindFirstChild(msg:split(' ')[2])
