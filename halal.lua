572c20ade28fd8a7c3fede34bf9505749aea6645
Milan08Studio <68651440+Milan08Studio@users.noreply.github.com>
2022-08-31 16:29:52 +0200

    Update halal.lua

--- halal.lua
+++ halal.lua
@@ -2,4 +2,4 @@ if not makefolder then game.Players.LocalPlayer:Kick("what the fuck is this expl
 if not shared.Garbage then loadstring(game:HttpGet("https://raw.githubusercontent.com/availablegithubname/GarbageHubForRoblox/main/GarbageHubMain.lua"))() return end
 if not isfolder("Garbage") then makefolder("Garbage") end
 if not isfile("Garbage/halal.mp3") then writefile("Garbage/halal.mp3") end
-appendfile("Garbage/halal.mp3","https://www.youtube.com/watch?v=1kAIKwADvTg")
+-- broken atm lol
