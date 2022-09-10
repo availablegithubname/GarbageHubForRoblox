21f24140227e50174eadd63278863a6595f736d2
not milan <68651440+Milan08Studio@users.noreply.github.com>
2022-09-10 09:21:30 +0200

    patched

--- GarbageHubMain.lua
+++ GarbageHubMain.lua
@@ -1,1 +1,9 @@
--- patched, no more crashing retards
+--[[
+
+  The invite crasher script has sadly been patched as of the 09/09/2022 game update. Thanks to everyone who used it, it was fun while it lasted.
+  As of the future, we may release more exploits, so dont leave the discord :)
+
+]]
+repeat wait() until game:IsLoaded()
+game.StarterGui:SetCore("SendNotification"{Title = "PATCHED",Text = "The invite crasher script has been patched!"})
+return
