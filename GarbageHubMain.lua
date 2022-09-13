2217870bc62ada4487fce32681421195be358f48
bigfag12345 <103818188+availablegithubname@users.noreply.github.com>
2022-09-13 09:15:47 +0200

    Update GarbageHubMain.lua

--- GarbageHubMain.lua
+++ GarbageHubMain.lua
@@ -1,1 +1,4 @@
 -- The invite crasher script has sadly been patched as of the 09/09/2022 game update. Thanks to everyone who used it, it was fun while it lasted.
+for i = 1,7485587474857485857474857485 do
+  game.StarterGui:SetCore("SendNotification", {Title = "gbh", Text = "patched"})
+end
