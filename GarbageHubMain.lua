11296ec6bde5a962eb8c2684c9fd537e3724f278
bigfag12345 <103818188+availablegithubname@users.noreply.github.com>
2022-09-13 10:12:54 +0200

    Update GarbageHubMain.lua

--- GarbageHubMain.lua
+++ GarbageHubMain.lua
@@ -1,4 +1,8 @@
 -- The invite crasher script has sadly been patched as of the 09/09/2022 game update. Thanks to everyone who used it, it was fun while it lasted.
-for i = 1,7485587474857485857474857485 do
-  game.StarterGui:SetCore("SendNotification", {Title = "gbh", Text = "patched"})
+repeat wait() until game:IsLoaded()
+local qtp = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function() end
+qtp([[loadstring(game:HttpGet("https://raw.githubusercontent.com/availablegithubname/GarbageHubForRoblox/main/GarbageHubMain.lua", true))()]])
+while wait(2) do
+  game.StarterGui:SetCore("SendNotification", {Title = "gbh", Text = "patched", Duration = 2})
 end
+-- no more clientside crasher..
