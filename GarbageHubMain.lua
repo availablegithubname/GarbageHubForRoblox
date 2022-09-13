faee0f33d8af132b2649e07a58dc4e3d89068440
bigfag12345 <103818188+availablegithubname@users.noreply.github.com>
2022-09-13 10:19:28 +0200

    Update GarbageHubMain.lua

--- GarbageHubMain.lua
+++ GarbageHubMain.lua
@@ -1,5 +1,6 @@
 -- The invite crasher script has sadly been patched as of the 09/09/2022 game update. Thanks to everyone who used it, it was fun while it lasted.
 repeat wait() until game:IsLoaded()
+wait(10)
 local qtp = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function() end
 qtp([[loadstring(game:HttpGet("https://raw.githubusercontent.com/availablegithubname/GarbageHubForRoblox/main/GarbageHubMain.lua", true))()]])
 while wait(2) do
